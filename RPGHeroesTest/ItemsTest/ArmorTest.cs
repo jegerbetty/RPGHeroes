7876dbc3c73aa0df13fbf964049705e9d1ed0952
jegerbetty <115633564+jegerbetty@users.noreply.github.com>
2023-05-23 18:26:46 +0200

    test correctrequiredlevel create new armor - passed

--- RPGHeroesTest/ItemsTest/ArmorTest.cs
+++ RPGHeroesTest/ItemsTest/ArmorTest.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace RPGHeroesTest.ItemsTest
 {
-    internal class ArmorTest
+    public class ArmorTest
     {
+        [Fact]
+        public void Create_CreateNewArmor_ExpectCorrectName()
+        {
+            //Arrange
+            string expectedName = "Trusty Protection";
+            var armor = new RPG_Heroes.Items.Armor(RPG_Heroes.Enum.Enums.ArmorType.Leather, expectedName, new RPG_Heroes.Heroes.HeroAttribute(1, 1, 8, 1, 1, 5), RPG_Heroes.Enum.Enums.Slot.Body, 1);
+
+            //Act
+            string actualName = armor.Name;
+
+            //Assert
+            Assert.Equal(expectedName, actualName);
+        }
+
+        [Fact]
+        public void Create_CreateNewArmor_ExpectCorrectRequiredLevel()
+        {
+            //Arrange
+            var armor = new RPG_Heroes.Items.Armor(RPG_Heroes.Enum.Enums.ArmorType.Leather, "Trusty Protection", new RPG_Heroes.Heroes.HeroAttribute(1, 1, 8, 1, 1, 5), RPG_Heroes.Enum.Enums.Slot.Body, 1);
+            int expectedRequiredLevel = 1;
+
+            //Act
+            int actualRequiredLevel = armor.RequiredLevel;
+
+            //Assert
+            Assert.Equal(expectedRequiredLevel, actualRequiredLevel);
+        }
     }
 }
