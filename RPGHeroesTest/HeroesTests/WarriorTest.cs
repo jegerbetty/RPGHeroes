4838fb344ea6643c8771ce8e8c7c8aa0b31542ba
jegerbetty <115633564+jegerbetty@users.noreply.github.com>
2023-05-23 18:58:32 +0200

    Same tests for Warrior as for the other subhero classes - same results
    two tests fail, results in Assert.Equal() fail, but the results (expected and actual) are the same.
    I think I should be overriding the Equals method, but I do not know how to do this yet.

--- RPGHeroesTest/HeroesTests/WarriorTest.cs
+++ RPGHeroesTest/HeroesTests/WarriorTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RPG_Heroes.Heroes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,65 @@ using System.Threading.Tasks;
 
 namespace RPGHeroesTest.HeroesTests
 {
-    internal class WarriorTest
+    public class WarriorTest
     {
+        [Fact]
+        public void Create_CreateNewWarrior_ExpectCorrectName()
+        {
+            //Arrange
+            string expectedName = "Harald";
+            var hero = new Warrior(expectedName);
+
+            //Act
+            string actualName = hero.Name;
+
+            //Assert
+            Assert.Equal(expectedName, actualName);
+
+        }
+
+        [Fact]
+        public void Create_CreateNewWarrior_ExpectCorrectLevel()
+        {
+            //Arrange
+            var hero = new Warrior("Harald");
+            int expectedLevel = 1;
+
+            //Act
+            int actualLevel = hero.Level;
+
+            //Assert
+            Assert.Equal(expectedLevel, actualLevel);
+        }
+
+        [Fact]
+        public void Create_CreateNewWarrior_ExpectCorrectAttributes()
+        {
+            //Arrange
+            var hero = new Warrior("Harald");
+            HeroAttribute expectedAttributes = new(5,2,1,3,2,1);
+
+            //Act
+            HeroAttribute actualAttributes = hero.HeroAttribute;
+
+            //Assert
+            Assert.Equal(expectedAttributes, actualAttributes); //Results in Assert.Equal() fail, but the results (expected and actual) are the same. 
+        }
+
+        [Fact]
+        public void LevelUp_WhenLevellingUpWarrior_ExpectCorrectIncreasedAttributes()
+        {
+            //Arrange
+            var hero = new Warrior("Harald");
+            HeroAttribute levelOneAttributes = hero.HeroAttribute;
+            HeroAttribute expectedLevelTwoAttributes = new(levelOneAttributes.Strength + 3, levelOneAttributes.Dexterity + 2, levelOneAttributes.Intelligence + 1, levelOneAttributes.LevelUpStrength, levelOneAttributes.LevelUpDexterity, levelOneAttributes.LevelUpIntelligence);
+
+            //Act
+            hero.LevelUp();
+            HeroAttribute actualLevelTwoAttributes = hero.HeroAttribute;
+
+            //Assert
+            Assert.Equal(expectedLevelTwoAttributes, actualLevelTwoAttributes); //Results in Assert.Equal() fail, but the results (expected and actual) are the same. 
+        }
     }
 }
