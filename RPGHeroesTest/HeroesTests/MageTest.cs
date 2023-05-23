fa17598d6590155c23b517ab834c1d01e87ac3ca
jegerbetty <115633564+jegerbetty@users.noreply.github.com>
2023-05-23 17:06:03 +0200

    Test for correct name for Mage - passed

--- RPGHeroesTest/HeroesTests/MageTest.cs
+++ RPGHeroesTest/HeroesTests/MageTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RPG_Heroes.Heroes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,22 @@ using System.Threading.Tasks;
 
 namespace RPGHeroesTest.HeroesTests
 {
-    internal class MageTest
+    public class MageTest
     {
+        [Fact]
+        public void Create_CreateNewMage_ExpectCorrectName()
+        {
+            //Arrange
+            string expectedName = "Alfred";
+            var hero = new Mage(expectedName);
+
+            //Act
+            string actualName = hero.Name;
+
+            //Assert
+            Assert.Equal(expectedName, actualName);
+
+        }
+        
     }
 }
